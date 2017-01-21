41d457894a3c0a20a93d540812e123f61542757f
Glen <daygle@users.noreply.github.com>
2017-01-21 15:46:00 +1100

    Update home.inc.php

--- include/pages/home.inc.php
+++ include/pages/home.inc.php
@@ -1,6 +1,7 @@
 <?php
 $defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 
+$smarty->assign("WEBSITE", $config['website']);
 $smarty->assign("SITESTRATUMURL", $config['gettingstarted']['stratumurl']);
 $smarty->assign("SITESTRATUMPORT", $config['gettingstarted']['stratumport']);
 $smarty->assign("SITECOINNAME", $config['gettingstarted']['coinname']);
