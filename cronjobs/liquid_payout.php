7e958c6501d1a66df7d869c59797b7899b26955c
Glen <daygle@daygle.net>
2017-06-10 20:36:40 +1000

    Correct bad English

--- cronjobs/liquid_payout.php
+++ cronjobs/liquid_payout.php
@@ -1,19 +1,24 @@
 #!/usr/bin/php
 <?php
+
 // Change to working directory
 chdir(dirname(__FILE__));
+
 // Include all settings and classes
 require_once('shared.inc.php');
+
 // Check For RPC Connection
 if ($bitcoin->can_connect() === true){
 $dBalance = $bitcoin->getbalance();
+
 // Check Wallet Balance
-$log->logDebug("The Wallet Balance is " .$dBalance. "\n");
+$log->logDebug("The wallet balance is " .$dBalance. "");
 $dGetInfo = $bitcoin->getinfo();
+
 // Check for POS Mint
 if (is_array($dGetInfo) && array_key_exists('newmint', $dGetInfo)) {
 $dNewmint = $dGetInfo['newmint'];
-$log->logDebug("Current Mint is: " .$dNewmint);
+$log->logDebug("Current mint is: " .$dNewmint );
 } else {
   $dNewmint = -1;
 }
@@ -22,27 +27,30 @@ $log->logDebug("Current Mint is: " .$dNewmint);
   $dNewmint = -1;
   $log->logError('Unable to connect to wallet RPC service');
 }
+
 // Fetch locked balance from transactions
 $dLockedBalance = $transaction->getLockedBalance();
-$log->logDebug("The Locked Wallet Balance for Users is: " .$dLockedBalance. "\n");
+$log->logDebug("The locked wallet balance for users is: " .$dLockedBalance. "");
+
 // Fetch Final Wallet Balance after Transfer
 $dFloat = $dLockedBalance + $config['coldwallet']['reserve'];
 $dThreshold = $config['coldwallet']['threshold'];
 $sendAddress = $config['coldwallet']['address'];
-$log->logDebug("The Locked Wallet Balance & Float amounts to: " .$dFloat. "\n");
+$log->logDebug("The locked wallet balance and float amounts to: " .$dFloat. "");
+
 // Send Liquid Balance
 $send = $dBalance - $dFloat ;
-$log->logInfo("Final Sending Amount is : " .$send. "\n");
+$log->logInfo("Liquid amount : " .$send. "");
 if($send > $dThreshold){
         if($sendAddress !== ''){
                 $bitcoin->sendtoaddress($sendAddress, $send);
         }
         else {
-                $log->logInfo("No wallet address set\n");
+                $log->logInfo("No wallet address set");
         }
 }
 else{
-        $log->logInfo("Final Sending Amount Not Exceed threshold : " .$send. "\n");
+        $log->logInfo("Liquid amount does not exceed the set threshold : " .$send. "");
 }
 
 // Monitoring cleanup and status update
